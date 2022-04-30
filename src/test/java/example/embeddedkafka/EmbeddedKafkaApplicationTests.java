c2b2637f50efa5ff65ceb8a0aeef0cf643b3f6a7
Lawrence Zeng <lawrence.zeng@egmail.com>
2022-04-30 11:37:38 -0400

    Regenerate project from Spring Initializer with Gradle for simpler dependency management

---
+++ src/test/java/example/embeddedkafka/EmbeddedKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package example.embeddedkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
