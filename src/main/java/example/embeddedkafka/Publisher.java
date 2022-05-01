c982b8e990db7e3dbaa8bb6d519491a64b2f687c
Lawrence Zeng <lawrence.zeng@egmail.com>
2022-05-01 10:15:54 -0400

    Add log for debug purpose

--- src/main/java/example/embeddedkafka/Publisher.java
+++ src/main/java/example/embeddedkafka/Publisher.java
@@ -1,5 +1,7 @@
 package example.embeddedkafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +10,14 @@ public class Publisher {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Publisher.class);
+
     public Publisher(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void send(String message) {
         this.kafkaTemplate.send("my-topic", message);
+        LOGGER.info("Message sent: " + message);
     }
 }
