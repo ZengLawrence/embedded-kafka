120ed63bb412248cc76624640b947585d57f039f
Lawrence Zeng <lawrence.zeng@gmail.com>
2022-05-08 14:46:40 -0400

    simply coding (#4)
    Co-authored-by: Lawrence Zeng <lawrence.zeng@egmail.com>

--- src/main/java/example/embeddedkafka/Publisher.java
+++ src/main/java/example/embeddedkafka/Publisher.java
@@ -1,18 +1,16 @@
 package example.embeddedkafka;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class Publisher {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-
-    public Publisher(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     public void send(String message) {
         this.kafkaTemplate.send("my-topic", message);
