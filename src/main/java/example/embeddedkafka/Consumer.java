57302f22f4af8a9b17a43456bd0bfc4aabccc11c
Lawrence Zeng <lawrence.zeng@gmail.com>
2022-05-08 12:00:13 -0400

    Lombok (#3)
    * Add lombok to simply coding
    
    Co-authored-by: Lawrence Zeng <lawrence.zeng@egmail.com>

--- src/main/java/example/embeddedkafka/Consumer.java
+++ src/main/java/example/embeddedkafka/Consumer.java
@@ -1,33 +1,26 @@
 package example.embeddedkafka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CountDownLatch;
 
 @Component
+@Slf4j
 public class Consumer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 
-    private final CountDownLatch latch = new CountDownLatch(1);
+    private @Getter final CountDownLatch latch = new CountDownLatch(1);
 
-    private String lastMessage;
+    private @Getter String lastMessage;
 
     @KafkaListener(topics = "my-topic", groupId = "consumer-group")
     public void processMessage(String content) {
-        LOGGER.info("Message received: " + content);
+        log.info("Message received: " + content);
         lastMessage = content;
         latch.countDown();
     }
 
-    public CountDownLatch getLatch() {
-        return latch;
-    }
-
-    public String getLastMessage() {
-        return lastMessage;
-    }
 }
