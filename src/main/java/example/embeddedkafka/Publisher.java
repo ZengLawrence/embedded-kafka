be4db6d6be16a33c9cf2a7799a2115825da894c3
Lawrence Zeng <lawrence.zeng@egmail.com>
2022-05-08 11:57:05 -0400

    Add lombok to simply coding

--- src/main/java/example/embeddedkafka/Publisher.java
+++ src/main/java/example/embeddedkafka/Publisher.java
@@ -1,16 +1,14 @@
 package example.embeddedkafka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class Publisher {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Publisher.class);
 
     public Publisher(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -18,6 +16,6 @@ public class Publisher {
 
     public void send(String message) {
         this.kafkaTemplate.send("my-topic", message);
-        LOGGER.info("Message sent: " + message);
+        log.info("Message sent: " + message);
     }
 }
